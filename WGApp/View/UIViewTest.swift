501eed75965faedd18b20184352a14065e7d95f4
Viviane Rehor <viviane.rehor@web.de>
2018-05-27 17:50:11 +0200

    test view folder

---
+++ WGApp/View/UIViewTest.swift
@@ -0,0 +1,21 @@
+//
+//  UIViewTest.swift
+//  WGApp
+//
+//  Created by Viviane Rehor on 27.05.18.
+//  Copyright © 2018 Viviane Rehor. All rights reserved.
+//
+
+import UIKit
+
+class UIViewTest: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
